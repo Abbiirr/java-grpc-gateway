1791b48959a4c293d6f04a491e137975b82e14db
Abir Hossain <abirhossain9@iut-dhaka.edu>
2024-12-08 16:55:37 +0600

    Init project

---
+++ src/main/java/org/example/javagrpcgateway/JavaGrpcGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.example.javagrpcgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGrpcGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaGrpcGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/javagrpcgateway/JavaGrpcGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javagrpcgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGrpcGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
